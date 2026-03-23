b0053cc6e7c3c77cafcd605bfe017ccde5f7631a
jayanthammanaboina432-ctrl <jayanthammanaboina432@gmail.com>
2026-03-23 14:54:14 +0530

    fixed login error

--- src/main/java/com/studygroup/repository/UserRepository.java
+++ src/main/java/com/studygroup/repository/UserRepository.java
@@ -1,7 +1,9 @@
-﻿package com.studygroup.repository;
+package com.studygroup.repository;
+
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.studygroup.model.User;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findFirstByEmail(String email);
 }
