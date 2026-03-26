fb86f26fa31fec8b716e953c7299df41e0d52806
jayanthammanaboina432-ctrl <jayanthammanaboina432@gmail.com>
2026-03-26 16:13:30 +0530

    Fix: Remove BOM from UserService.java

--- src/main/java/com/studygroup/service/UserService.java
+++ src/main/java/com/studygroup/service/UserService.java
@@ -1,4 +1,4 @@
-﻿package com.studygroup.service;
+package com.studygroup.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.studygroup.model.User;
